71102c0856fb0ff5c5db39a72068d0ab19eeb9e3
shafeeque03 <129934317+shafeeque03@users.noreply.github.com>
2024-02-06 22:06:16 +0530

    final hosting commit

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const connectDB = require('./Config/Database')
 const socketConnection = require('./socketio')
 
 connectDB.dbconnect()
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
     origin:"https://roomque-room.vercel.app",
     methods:['GET','POST','PUT','PATCH'],
