49b68cbd0dc3ab00939936edfed24f01f3b426d8
shafeeque03 <129934317+shafeeque03@users.noreply.github.com>
2024-02-06 20:41:39 +0530

    MongoClusterChanged

--- Config/Database.js
+++ Config/Database.js
@@ -2,17 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 
 module.exports = {
-    dbconnect: () => {
-        console.log("Connecting to MongoDB:", process.env.MONGO_URL);
-        mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
+    dbconnect:()=>{
+        mongoose.connect(process.env.MONGO_URL)
+        .then(()=>{
             console.log("DB Connected");
         })
-        .catch((err) => {
-            console.log("Error connecting to MongoDB:", err);
-        });
+        .catch((err)=>{
+            console.log(err);
+        })
     }
-};
+}
