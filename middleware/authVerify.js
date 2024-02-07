eb9f7f38c4d4f527cf3962ec00e93599a713941d
shafeeque03 <129934317+shafeeque03@users.noreply.github.com>
2024-02-07 16:35:20 +0530

    secretKey committm

--- middleware/authVerify.js
+++ middleware/authVerify.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const secretKey = process.env.SECRET_KEY
+const secretKey = mysecretkeyforjwt
 const User = require("../Model/userModel")
 const Owner = require("../Model/ownerModel")
 
