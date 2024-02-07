03a7fe35bded6c8ab20f7e0ef73d469df31010bc
shafeeque03 <129934317+shafeeque03@users.noreply.github.com>
2024-02-07 16:25:13 +0530

    host committm

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ connectDB.dbconnect()
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-    origin:"http://localhost:5173",
-    // origin:"https://roomque-room.vercel.app",
+    // origin:"http://localhost:5173",
+    origin:"https://roomque-room.vercel.app",
     methods:['GET','POST','PUT','PATCH'],
     credentials:true
 }));
